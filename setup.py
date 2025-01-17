e6098bb91995b9434622914cb9e1e024fd1de528
mgarber21 <53231272+mgarber21@users.noreply.github.com>
2025-01-16 17:56:13 -0800

    uploading additional files

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 from setuptools import setup, find_packages
 
-# Dynamically read requirements.txt
-with open("requirements.txt", "r") as f:
-    requirements = [line.strip() for line in f if line.strip() and not line.startswith("#")]
-
 setup(
     name="WonderPeaks_preprocessing",
-    version="0.1.0",
+    version="0.2.3",  # Match the version in pyproject.toml
+    description="Preprocessing functions for ChIP-seq and RNA-seq in Fungi with WonderPeaks",
+    author="Megan E. Garber",
+    author_email="mgarber21@gmail.com",
+    url="https://github.com/mgarber21/WonderPeaks_preprocessing",
+    license="MIT",
     packages=find_packages(),
-    install_requires=requirements,  # Dependencies are loaded dynamically
+    include_package_data=True,  # Enables MANIFEST.in
+    install_requires=[
+        "deepTools==3.5.1",
+        "deeptoolsintervals==0.1.9",
+        "multiqc==1.10.1",
+        ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
